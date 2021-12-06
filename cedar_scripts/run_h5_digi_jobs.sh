9feac45fd23d6b0edca78731b46eac402d3aee50
Joanna-Gao <joannagao6918@gmail.com>
2021-12-06 09:21:53 -0800

    temp

--- cedar_scripts/run_h5_digi_jobs.sh
+++ cedar_scripts/run_h5_digi_jobs.sh
@@ -11,7 +11,7 @@ cd $DATATOOLS/cedar_scripts
 # project name, input data directory and output file name for this run
 name=HKHybrid
 data_dir=/scratch/jgao/data/
-output_dir=/scratch/jgao/data/HKHybrid/h5
+output_dir=${data_dir}/${name}/h5
 mkdir -p $output_dir
 
 log_dir="/scratch/jgao/log/h5"
@@ -21,7 +21,7 @@ cd $log_dir
 # 0-999 npz files for mu-, 0-999 npz files for e-
 # 0-9 in 1 file, 10-99 in 1 file, 100-199 ... 900-999.
 for i in {1..9}; do
-  for j in mu- pi0; do
+  for j in e- mu- pi0; do
     f="${data_dir}/${name}/${j}/*/*/*/*_${i}[0-9][0-9].npz "
     sbatch --time=3:0:0 --job-name=npz2h5_${j}_${i} \
       "${DATATOOLS}/cedar_scripts/make_digihit_h5.sh" \

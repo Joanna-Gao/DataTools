c71a2dc3db2aedbb042c797071dbc7a4218fedb9
Joanna-Gao <joannagao6918@gmail.com>
2022-03-13 14:59:02 -0700

    temp

--- cedar_scripts/run_merge_jobs.sh
+++ cedar_scripts/run_merge_jobs.sh
@@ -19,8 +19,8 @@ mkdir -p $log_dir
 cd $log_dir
 
 # process all the h5 files
-f="${data_dir}/*.hdf5"
+# f="${data_dir}/*.hdf5"
 sbatch --time=3:0:0 --job-name=mergeh5_all \
   "${DATATOOLS}/cedar_scripts/merge_h5_script.sh" \
-  "${output_dir}/gamma.hdf5" "$f"
+  "${output_dir}/all_w_gamma.hdf5" "${output_dir}/gamma.hdf5" "${output_dir}/all.hdf5" #"$f"
 

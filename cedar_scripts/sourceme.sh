55e73f890c6ac05a00acb59c585e3ffe62e62f61
Joanna-Gao <joannagao6918@gmail.com>
2023-08-31 07:33:06 -0700

    Modified envs and batch submission file for new data production

--- cedar_scripts/sourceme.sh
+++ cedar_scripts/sourceme.sh
@@ -3,10 +3,14 @@ module load StdEnv/2016
 module load gcc/6.4.0
 module load python/3.6.3
 module load scipy-stack
-source /project/rpp-blairt2k/machine_learning/production_software/root/install/bin/thisroot.sh
-source /project/rpp-blairt2k/machine_learning/production_software/Geant4/geant4.10.01.p03-install/share/Geant4-10.1.3/geant4make/geant4make.sh
-export G4WORKDIR=/project/rpp-blairt2k/machine_learning/production_software/WCSim/exe
+source /project/rpp-blairt2k/machine_learning/production_software/root/install-2021-03-26/bin/thisroot.sh
+export GEANT4_BASE_DIR=/project/rpp-blairt2k/machine_learning/production_software/Geant4/geant4.10.03.p03-install
+source ${GEANT4_BASE_DIR}/share/Geant4-10.3.3/geant4make/geant4make.sh
+# source /scratch/jgao/software/root/install/bin/thisroot.sh
+# source /scratch/jgao/software/geant4.10.03.p03/install/share/Geant4-10.3.3/geant4make/geant4make.sh
+export G4WORKDIR=/project/rpp-blairt2k/jgao/WCSim/exe
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}${G4LIB}/${G4SYSTEM}
-export WCSIMDIR=/project/rpp-blairt2k/machine_learning/production_software/WCSim
+export WCSIMDIR=/project/rpp-blairt2k/jgao/WCSim
 export DATATOOLS="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
+export FITQUN_ROOT=/project/rpp-blairt2k/prouse/hk/fiTQun
 export PYTHONPATH=$DATATOOLS:$PYTHONPATH
--- cedar_scripts/submit_jobs.sh
+++ cedar_scripts/submit_jobs.sh
@@ -6,20 +6,19 @@ set -e
 
 # initial environment setup
 # export DATATOOLS=/project/rpp-blairt2k/machine_learning/production_software/DataTools
+echo "Working ok"
 export DATATOOLS=/project/rpp-blairt2k/jgao/DataTools
 export PYTHONPATH=$DATATOOLS:$PYTHONPATH
-source /project/rpp-blairt2k/jgao/sourceme.sh
+# source /project/rpp-blairt2k/jgao/sourceme.sh
 
 cd $DATATOOLS/cedar_scripts
 
 # name and output data directory for this run
-name=HKHybrid
-data_dir=/project/rpp-blairt2k/machine_learning/data
-# data_dir=/scratch/jgao/data
+name=WCSim_test
+data_dir=/scratch/jgao/data
 
 # Run setup scripts
-# source setup_jobs.sh "$name" "$data_dir"
-source "${data_dir}/${name}/sourceme.sh"
+source setup_jobs.sh "$name" "$data_dir"
 
 # set directory where log files will be saved
 export LOGDIR="/scratch/$USER/log/$name/"
@@ -30,4 +29,4 @@ cd "$LOGDIR"
 # submit jobs with desired options
 # JOBTIME=`date` sbatch --time=0:59:00 --array=0-9 --job-name=e "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 100 -e 100 -E 1000 -P e- -d 2pi -p fix -x 0 -y 0 -z 0
 # JOBTIME=`date` sbatch --time=0:59:00 --array=0-9 --job-name=mu "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 100 -e 100 -E 1000 -P mu- -d 2pi -p fix -x 0 -y 0 -z 0
-JOBTIME=`date` sbatch --time=1-20:00:00 --mem-per-cpu=16G --array=4,8,590 --job-name=wcs_hkh_g "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 3000 -e 0 -E 1000 -P gamma -o z -d 4pi -p unif -y 3287 -R 3240 -g HyperK_HybridmPMT -m
+JOBTIME=`date` sbatch --time=0:59:00 --array=0 --job-name=e "$DATATOOLS/cedar_scripts/run_WCSim_job.sh" "$name" "$data_dir" -n 100 -e 100 -E 1000 -P e- -d 2pi -p fix -x 0 -y 0 -z 0
